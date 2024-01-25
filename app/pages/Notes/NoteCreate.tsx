a64b4f474b398a29b51a5fe89482277be67b1639
ananduremanan <ananduor@gmail.com>
2024-01-25 16:08:14 +0530

    Updated Logic in Adding Notes

--- app/pages/Notes/NoteCreate.tsx
+++ app/pages/Notes/NoteCreate.tsx
@@ -3,41 +3,82 @@ import { Body1, Body1Strong, Button } from "@fluentui/react-components";
 import { mdiEarth } from "@mdi/js";
 import Icon from "@mdi/react";
 import React, { useState, useEffect } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
-// import { appDataDir } from "@tauri-apps/api/path";
+import { BaseDirectory, writeTextFile } from "@tauri-apps/api/fs";
+import {
+  useId,
+  Toaster,
+  useToastController,
+  Toast,
+  ToastTitle,
+  ToastBody,
+} from "@fluentui/react-components";
+import { Store } from "tauri-plugin-store-api";
 
-export const NoteCreate = () => {
+export const NoteCreate = ({ userNotes }: any) => {
   const [date, setDate] = useState("");
-  const [title, setTitle] = useState("Your Title");
+  const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const [appDataDirectory, setAppDataDirectory] = useState<any>();
+  const [notes, setNotes] = useState<any[]>([]);
+  const [localNotes, setLocalNotes] = useState<any[]>([]);
 
-  async function setAppDataDir() {
-    const appDataDir = (await import("@tauri-apps/api/path")).appDataDir;
-    setAppDataDirectory(appDataDir);
-  }
+  const toasterId = useId("toaster");
+  const { dispatchToast } = useToastController(toasterId);
+
+  const getNotes = async () => {
+    const val = await store.get("_noteData");
+    userNotes(val);
+  };
+
+  const store = new Store(".noteData.json");
+
+  const _userSetNotes = async () => {
+    await store.set("_noteData", notes);
+    await store.save();
+  };
+
+  useEffect(() => {
+    _userSetNotes();
+    getNotes();
+  }, [notes]);
 
   useEffect(() => {
     const today: Date = new Date();
     setDate(today.toString().split(" GMT")[0]);
-    setAppDataDir();
+    // getNotes();
   }, []);
 
-  const handleSave = async () => {
-    const noteObject = {
-      id: Math.random().toString().substring(2, 10),
-      title: title,
-      body: body,
-      createdAt: date,
-    };
-    const appDataDirPath = await appDataDirectory;
-    const filePath = `${appDataDirPath}/notes.json`;
+  const notify = () =>
+    dispatchToast(
+      <Toast>
+        <ToastTitle>Note Added</ToastTitle>
+        <ToastBody subtitle="Subtitle">Note Added Successfully</ToastBody>
+      </Toast>,
+      { intent: "success" }
+    );
 
-    
+  const handleSave = async () => {
+    try {
+      const fileName: string = Math.random()
+        .toString()
+        .substring(2, 10)
+        .toString();
+      const folderName: string = "noteData";
+      await writeTextFile(`${folderName}/${fileName}.txt`, body, {
+        dir: BaseDirectory.Document,
+      });
+      notify();
+      const noteData: any = { fileId: fileName, title: title };
+      setNotes([{ ...noteData }, ...notes]);
+      setTitle("");
+      setBody("");
+    } catch (error) {
+      console.error("Error Occured: ", error);
+    }
   };
 
   return (
     <div className="w-full ml-4">
+      <Toaster toasterId={toasterId} />
       <div className="flex flex-row justify-between w-full">
         <div>
           <Body1Strong>{title}</Body1Strong>
@@ -59,6 +100,7 @@ export const NoteCreate = () => {
               onInput={(event: any) => {
                 setTitle(event.target.value);
               }}
+              value={title}
             />
           </div>
           <div className="flex gap-2">
@@ -73,6 +115,7 @@ export const NoteCreate = () => {
             onInput={(event: any) => {
               setBody(event.target.value);
             }}
+            value={body}
           />
         </div>
       </div>
